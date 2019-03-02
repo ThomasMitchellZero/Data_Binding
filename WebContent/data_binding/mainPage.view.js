1cea9c33b676da35f1366df42b56cb9d2bb31c0e
ThomasMitchellZero <thomas.mitchell.13@gmail.com>
2019-03-02 11:34:27 -0500

    Initial commit

---
+++ WebContent/data_binding/mainPage.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("data_binding.mainPage", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf data_binding.mainPage
+	*/ 
+	getControllerName : function() {
+		return "data_binding.mainPage";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf data_binding.mainPage
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
